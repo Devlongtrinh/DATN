e6496fa4384bf6df5a9605a4f6ffd8472fff0a2f
TQL Long <tenlong201@gmail.com>
2024-12-16 23:20:49 +0700

    commit 15/12

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -8,7 +8,7 @@
 
                 <!-- Logo desktop -->
                 <a href="/" class="logo">
-                    <img src="/template/images/icons/logo.png" alt="IMG-LOGO">
+                    <img src="/template/images/icons/OIP.jpg" alt="IMG-LOGO">
                 </a>
 
                 <!-- Menu desktop -->
